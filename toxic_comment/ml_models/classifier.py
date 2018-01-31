2cbde736801d48986f124dbea8b485c10c41ed49
Mohit Rohatgi <Moit.Rohatgi@ipsoft.com>
2018-01-31 14:57:45 +0530

    added to string method to toxicity

--- toxic_comment/ml_models/classifier.py
+++ toxic_comment/ml_models/classifier.py
@@ -1,10 +1,17 @@
 from toxic_comment.models import toxicity
 
-class Classifier(object):
-    __metaclass__ = toxicity
 
+class Classifier(object):
+
+    """
+    Given a training file path and delimiter, this method should train the model.
+    Assumption is only one delimiter is used for separation of data
+    """
     def train(self, train_file_path, delimiter):
         raise NotImplementedError("training api not implemented")
 
+    """
+    Given a test file path, compute the output and reeturn the object toxicity.
+    """
     def test(self, test_file_path) -> toxicity:
         raise NotImplementedError("testing api not implemented")
--- toxic_comment/models/toxicity.py
+++ toxic_comment/models/toxicity.py
@@ -1,7 +1,8 @@
 class Toxicity(object):
-    def __init__(self):
-        self.id = []
+    def __init__(self, delimiter):
+        self.ids = []
         self.result = {}
+        self.delimiter = delimiter
 
     def add_result(self, id, toxic,severe_toxic,obscene,threat,insult,identity_hate):
         assert toxic <= 1.0
@@ -10,5 +11,16 @@ class Toxicity(object):
         assert threat <= 1.0
         assert insult <= 1.0
         assert identity_hate <= 1.0
-        self.id.append(id)
+        self.ids.append(id)
         self.result[id] = [toxic,severe_toxic,obscene,threat,insult,identity_hate]
+
+    def __str__(self):
+        output = ""
+        for id in self.ids:
+            output += id + self.delimiter + self._get_concatenated(id) + "\n"
+        return output
+
+    def _get_concatenated(self, id):
+        output = self.result[id].pop(0)
+        while len(self.result[id]) > 0:
+            output += self.delimiter + self.result[id].pop(0)
