de9d37fa001822f9de6356ac3f5351161a9cc8b5
mohitRohatgi <mohit.rohatgi.mec10@itbhu.ac.in>
2018-02-01 17:38:54 +0530

    refactored hard coding

--- toxic_comment/main/models/data_points.py
+++ toxic_comment/main/models/data_points.py
@@ -10,6 +10,7 @@ class DataPoints(object):
         self.train = None
         self.test = None
         self.valid = None
+        self.split_distribution = (0.6, 0.8, 1.0)
 
     """
     This method adds a data point into the class.
@@ -28,9 +29,37 @@ class DataPoints(object):
         self.ids = np.array(self.ids)
         np.random.shuffle(self.ids)
         size = len(self.ids)
-        train_end = round(0.6 * size)
-        valid_end = round(0.8 * size)
+        train_end = round(self._get_train_split() * size)
+        valid_end = round(self._get_valid_split() * size)
         self.train = self.ids[:train_end]
         self.valid = self.ids[train_end:valid_end]
         self.test = self.ids[valid_end:]
         return self
+
+    def get_train_id2text(self):
+        return self._get_sub_dict(self.train, self.id2text)
+
+    def get_train_id2_label(self):
+        return self._get_sub_dict(self.train, self.id2label)
+
+    def get_valid_id2text(self):
+        return self._get_sub_dict(self.valid, self.id2text)
+
+    def get_valid_id2_label(self):
+        return self._get_sub_dict(self.valid, self.id2label)
+
+    def get_test_id2text(self):
+        return self._get_sub_dict(self.test, self.id2text)
+
+    def get_test_id2_label(self):
+        return self._get_sub_dict(self.test, self.id2label)
+
+    @staticmethod
+    def _get_sub_dict(keys, dict):
+        return {k: dict[k] for k in keys}
+
+    def _get_train_split(self):
+        return self.split_distribution[0]
+
+    def _get_valid_split(self):
+        return self._get_train_split() + self.split_distribution[1]
