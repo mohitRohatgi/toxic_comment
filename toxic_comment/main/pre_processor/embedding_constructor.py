1f392858fd47795877916e18e1c5267b1e86bf64
mohitRohatgi <mohit.rohatgi.mec10@itbhu.ac.in>
2018-02-07 10:52:15 +0530

    building of vocabs remaining

--- toxic_comment/main/pre_processor/embedding_constructor.py
+++ toxic_comment/main/pre_processor/embedding_constructor.py
@@ -1,5 +1,14 @@
 import yaml
 import numpy as np
+from os.path import isfile
+import pickle
+
+"""
+embed path : embeddings file path. eg. glove vectors
+embedding matrix path : path of the embedding matrix constructed
+embedding: embedding loaded from embed_path file. e.g. glove vectors
+embedding matrix: embedding matrix constructed from embeddings
+"""
 
 
 class EmbeddingConstructor:
@@ -9,8 +18,11 @@ class EmbeddingConstructor:
         self.embed_path = self.paths['embed_path']
         self.train_path = self._get_abs_path(self.paths['train_path'])
         self.test_path = self._get_abs_path(self.paths['test_path'])
-        self.word2Id_path = self._get_abs_path('word2Id_path')
-        self.id2Word_path = self._get_abs_path('id2Word_path')
+        self.word2Id_path = self._get_abs_path(self.paths['word2Id_path'])
+        self.id2Word_path = self._get_abs_path(self.paths['id2Word_path'])
+        self.embed_matrix_path = self._get_abs_path(self.paths['embed_matrix_path'])
+        self.word2Id = None
+        self.id2Word = None
         self.embed_matrix = None
         self.unknown = 'unk'
         self._load_vocab_dicts_and_embed_matrix()
@@ -38,19 +50,22 @@ class EmbeddingConstructor:
         return embedding
 
     def _load_vocab_dicts_and_embed_matrix(self):
-        if self._vocab_dicts_created():
-            self._load_vocab_dicts()
+        if self._vocab_dicts_and_matrix_created():
+            self._load_vocab_and_matrix()
         else:
-            self._build_and_save_vocab()
-
-        if self.embed_matrix_created():
-            self._load_embed_matrix()
-        else:
-            self._build_and_save_embed_matrix()
+            self._build_and_save_vocab_and_matrix()
 
     def _build_and_save_vocab(self):
         pass
 
-    def _build_and_save_embed_matrix(self):
-        self.embed_matrix = self._build_embed_matrix()
-        return self._save_embed_matrix()
+    def _vocab_dicts_and_matrix_created(self):
+        return isfile(self.word2Id_path) and isfile(self.id2Word_path) and isfile(self.embed_matrix_path)
+
+    def _load_vocab_and_matrix(self):
+        self.word2Id = pickle.load(self.word2Id_path)
+        self.id2Word = pickle.load(self.id2Word_path)
+        self.embed_matrix = pickle.load(self.embed_matrix_path)
+        assert (self.word2Id is not None and self.id2Word is not None and self.embed_matrix is not  None)
+
+    def _build_and_save_vocab_and_matrix(self):
+        pass
